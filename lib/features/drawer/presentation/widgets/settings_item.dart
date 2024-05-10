efefe27e6ddf7d90376a6cafeadb392280c0dcdd
Eldeengawy <eldengaawy@gmail.com>
2024-05-10 21:51:51 +0300

    Add SettingsItem widget for drawer settings
    Implement a new SettingsItem widget for displaying settings in the drawer.
    This widget includes title, icon, color, and optional new indicator.

---
+++ lib/features/drawer/presentation/widgets/settings_item.dart
@@ -0,0 +1,58 @@
+import 'package:chat_gpt/core/extensions/sized_box.dart';
+import 'package:chat_gpt/core/static/app_styles.dart';
+import 'package:chat_gpt/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SettingsItem extends StatelessWidget {
+  final String title;
+  final String icon;
+  final Color? color;
+  final bool? isNew;
+  const SettingsItem({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.color = AppColors.white,
+    this.isNew = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset(
+                icon,
+                colorFilter: ColorFilter.mode(color!, BlendMode.srcIn),
+              ),
+              16.pw,
+              Text(
+                title,
+                style: AppStyles.medium16white.copyWith(color: color),
+              ),
+            ],
+          ),
+          if (isNew!) ...[
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 8),
+              decoration: BoxDecoration(
+                color: AppColors.yellowFB,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                'NEW',
+                style: AppStyles.semiBold12white
+                    .copyWith(color: AppColors.green88),
+              ),
+            )
+          ]
+        ],
+      ),
+    );
+  }
+}
