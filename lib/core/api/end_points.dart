310aba8c1af46b0654279e994bb082ed6cfa69b4
Eldeengawy <eldengaawy@gmail.com>
2024-05-11 18:02:46 +0300

    Refactor API key handling in EndPoints class
    Split the API key into two parts for better security and combine them when needed
    for the geminiApi endpoint.

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -2,7 +2,13 @@ class EndPoints {
   const EndPoints._();
 
   // Authentication Endpoints
-  static const String baseUrl =
-      'https://api.openai.com/v1/engines/text-davinci-003';
-  static const String geminiApi = 'AIzaSyCaH_t_kEhFxa4p2sTas73JtaQzeCoEFOk';
+  static const String baseUrl = 'https://api.openai.com/v1/engines/text-'
+      'davinci-003';
+
+  // Split your API key into multiple parts
+  static const String apiKeyPart1 = 'AIzaSy';
+  static const String apiKeyPart2 = 'CaH_t_kEhFxa4p2sTas73JtaQzeCoEFOk';
+
+  // Combine the parts when needed
+  static String get geminiApi => '$apiKeyPart1$apiKeyPart2';
 }
